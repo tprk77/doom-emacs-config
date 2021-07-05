8bb3086656773fae4c476440b2a83195b1acdbff
Tim Perkins <code@tprk77.net>
2021-07-05 15:40:31 -0400

    Disable snippets
    The default ones are not so useful to me and I'm not even sure how to
    edit them, or if that's even possible.

--- init.el
+++ init.el
@@ -60,7 +60,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       ;snippets          ; Just too much, too much
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
