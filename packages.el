04a6467722c3ccf94a38208fe99a1146d760176f
Tim Perkins <code@tprk77.net>
2021-08-03 22:31:47 -0400

    Add rg for better ripgrep usage

--- packages.el
+++ packages.el
@@ -49,6 +49,8 @@
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
 
+(package! rg)
+
 (package! google-c-style
   :recipe (:host github
            :repo "google/styleguide"
