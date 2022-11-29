36b44ffde51c501c10ad6b247686dce48b46eeb2
Tim Perkins <code@tprk77.net>
2022-11-28 19:38:16 -0500

    Increase font size for 4k monitors

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 14)
-      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 14))
+(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 20)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 20))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
