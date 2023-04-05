0cc1b94560785d86f43d2368ceae7dfcc13f6b5d
Tim Perkins <code@tprk77.net>
2023-04-05 04:36:20 +0000

    Add workarounds for broken packages
    Apparently Magit is using Emacs 29 things, which is breaking Doom, which
    is still on Emacs 28.
    
    See: https://emacs.stackexchange.com/a/75836

--- packages.el
+++ packages.el
@@ -58,3 +58,13 @@
            :files ("*.el")))
 
 (package! docker-tramp)
+
+;; Workaround: (void-variable transient-base-map)
+(package! transient
+  :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
+  :recipe (:host github :repo "magit/transient"))
+
+;; Workaround: (void-function defvar-keymap)
+(package! with-editor
+  :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
+  :recipe (:host github :repo "magit/with-editor"))
